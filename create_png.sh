1cde9c5508f4b3a16e43dc25af75a5182332dd00
Bartosz Jarocki <jarocki.bartek@gmail.com>
2017-03-09 22:00:39 +0100

    Update create_png.sh

--- create_png.sh
+++ create_png.sh
@@ -2,7 +2,6 @@
 set -e
 
 init_folder() {
-    [ -e $1 ] && rm -r $1
     mkdir -p $1
 }
 
@@ -17,23 +16,29 @@ cp -a png/. temp
 cd temp
 
 for file in $(find . -type f -iname '*@4x*'); do
+  rm "../../app/src/main/res/drawable-xxxhdpi/${file/@4x/}" ||:
   mv "$file" "../../app/src/main/res/drawable-xxxhdpi/${file/@4x/}"
 done
 
 for file in $(find . -type f -iname '*@3x*'); do
+  rm "../../app/src/main/res/drawable-xxhdpi/${file/@3x/}" ||:
   mv "$file" "../../app/src/main/res/drawable-xxhdpi/${file/@3x/}"
 done
 
 for file in $(find . -type f -iname '*@2x*'); do
+  rm "../../app/src/main/res/drawable-xhdpi/${file/@2x/}" ||:
   mv "$file" "../../app/src/main/res/drawable-xhdpi/${file/@2x/}"
 done
 
 for file in $(find . -type f -iname '*@1,5x*'); do
+  rm "../../app/src/main/res/drawable-hdpi/${file/@1,5x/}" ||:
   mv "$file" "../../app/src/main/res/drawable-hdpi/${file/@1,5x/}"
 done
 
 for file in $(find . -type f -iname '*@1x*'); do
+  rm "../../app/src/main/res/drawable-mdpi/${file/@1x/}" ||:
   mv "$file" "../../app/src/main/res/drawable-mdpi/${file/@1x/}"
 done
 
 rm -r ../temp
+
